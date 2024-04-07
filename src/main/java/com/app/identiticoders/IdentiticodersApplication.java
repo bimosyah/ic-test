332713626b3a55aee20c5a4a42285e20c5baddff
Bimo Syahputro <>
2024-04-07 09:21:48 +0700

    init project

---
+++ src/main/java/com/app/identiticoders/IdentiticodersApplication.java
@@ -0,0 +1,13 @@
+package com.app.identiticoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentiticodersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentiticodersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/identiticoders/IdentiticodersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.identiticoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentiticodersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

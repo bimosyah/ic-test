24e94181c6d81b4000157e66f690bf94399e84a7
Bimo Syahputro <>
2024-06-17 09:07:25 +0700

    v1.0.0

--- src/main/java/com/app/identiticoders/controllers/impl/GeneralControllerImpl.java
+++ src/main/java/com/app/identiticoders/controllers/impl/GeneralControllerImpl.java
@@ -3,6 +3,7 @@ package com.app.identiticoders.controllers.impl;
 import com.app.identiticoders.controllers.GeneralController;
 import com.app.identiticoders.responses.BaseResponse;
 import com.app.identiticoders.utils.ResponseHelper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GeneralControllerImpl implements GeneralController {
 
+    @Value(value = "${spring.application.version}")
+    private String appVersion;
+
     @Override
     public ResponseEntity<BaseResponse> ping() {
-        return ResponseHelper.buildOkResponse("PONG");
+        return ResponseHelper.buildOkResponse("PONG " + appVersion);
     }
 
     @Override
